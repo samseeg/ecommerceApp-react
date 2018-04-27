c5eba755ce169f7f405952cf10f067d3a4ed5086
sambseeg <samseegmiller@gmail.com>
2018-04-27 10:37:58 -0600

    early basic layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import router from './router';
-import './App.css';
 
 class App extends Component {
   render() {
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,2 +1,35 @@
-import React, {Component} from 'react';
-import Link from 'react-router-dom';
+import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+import './Main.css';
+
+class Main extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            cart: []
+        }
+    }
+
+
+    render() {
+        return (
+            <div className='main_wrapper'>
+                <div className='head'>
+                    <div className='ham_menu'>
+
+                    </div>
+
+                    <div className='title'>
+
+                    </div>
+
+                    <div className='cart'>
+                    
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+export default Main;
